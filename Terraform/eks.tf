93d478f4417ecaa70164c5e418f693eb7d09adcb
nayem9b <razwanniam1@gmail.com>
2026-01-20 23:19:15 +0600

    managed node group for eks

--- Terraform/eks.tf
+++ Terraform/eks.tf
@@ -21,7 +21,7 @@ module "eks" {
   subnet_ids               = module.vpc.private_subnets
   # control_plane_subnet_ids = module.vpc.intra_subnets
 
-  # # EKS Managed Node Group(s)
+  
   # eks_managed_node_group_defaults = {
   #   ami_type       = "AL2_x86_64"
   #   instance_types = ["m5.large"]
